951ecee6435aac9f15e6bd53d3e11804a674bf1c
PandaKata <83578130+PandaKata@users.noreply.github.com>
2023-03-10 20:29:11 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,1 +1,30 @@
+locals {
+  data_lake_bucket = "capstone_datalake"
+}
 
+variable "project" {
+  description = "capstone-project-379718"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "europe-west6"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "covid_data_all"
+}
+
+variable "TABLE_NAME" {
+  description = "BigQuery Table that raw data (from GCS) will be written to"
+  type = string
+  default = "covid_data"
+}
