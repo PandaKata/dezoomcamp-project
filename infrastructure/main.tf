e5fabc28b38d9ac34b532422b58b3ee73b206bf2
PandaKata <83578130+PandaKata@users.noreply.github.com>
2023-03-10 20:34:29 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,3 @@
-
-49 lines (42 sloc)  1.28 KB
-
 terraform {
   required_version = ">= 1.0"
   backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
