849df80c7099a188d78ea1ff254b049a509b651b
kataeichinger@gmail.com <kataeichinger@gmail.com>
2023-03-24 18:28:23 +0000

    added columns

--- dbt/models/staging/stg_covid_project.sql
+++ dbt/models/staging/stg_covid_project.sql
@@ -1,6 +1,22 @@
 {{config(materialized='view') }}
 
-select *
+select 
+
+    -- identifiers
+    cast(iso_code as string) as country_code,
+    cast(continent as string) as continent,
+    cast(location as string) as  location,
+
+    --date
+    cast(date as datetime) as date,
+    
+    -- numbers
+    cast(total_cases as float64) as total_cases,
+    cast(new_cases as float64) as new_cases,
+    cast(new_cases_smoothed as float64) as new_cases_smoothed,
+    
+    
+
 
 
 from {{ source('staging', 'covid_table') }}
