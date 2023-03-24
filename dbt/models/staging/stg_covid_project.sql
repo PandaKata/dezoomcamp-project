bf2a4943e48c13c16b754117ed40dd411a98fb46
kataeichinger@gmail.com <kataeichinger@gmail.com>
2023-03-24 19:11:34 +0000

    added new .sql for germany

--- dbt/models/staging/stg_covid_project.sql
+++ dbt/models/staging/stg_covid_project.sql
@@ -14,8 +14,15 @@ select
     cast(total_cases as float64) as total_cases,
     cast(new_cases as float64) as new_cases,
     cast(new_cases_smoothed as float64) as new_cases_smoothed,
-    
-    
+    cast(total_deaths as float64) as total_deaths,
+    cast(new_deaths as float64) as new_deaths,
+    cast(new_deaths_smoothed as float64) as new_deaths_smoothed,
+    cast(total_cases_per_million as float64) as total_cases_per_million,
+    cast(new_cases_per_million as float64) as new_cases_per_million,
+    cast(new_deaths_smoothed_per_million as float64) as new_deaths_smoothed_per_million,
+    cast(total_deaths_per_million as float64) as total_deaths_per_million,
+    cast(new_deaths_per_million as float64) as new_deaths_per_million,
+    cast()
 
 
 
