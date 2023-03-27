8a81825b650b4e33cf06062d0f0a083d6108af02
kataeichinger@gmail.com <kataeichinger@gmail.com>
2023-03-27 18:22:28 +0000

    added production models

---
+++ dbt/models/core/prod_covid_ger.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "date",
+      "data_type": "timestamp",
+      "granularity": "day"
+    }
+)}}
+
+with covid_data as (
+    select *, 
+    from {{ ref('stg_covid_project') }}
+    where (country = 'Germany') and (date is not null) 
+
+) 
+
+select 
+    extract(YEAR from covid_data.date) as year, 
+    extract(MONTH from covid_data.date) as month,
+    covid_data.date, 
+    covid_data.new_cases_per_mio as new_cases_per_mio,
+    covid_data.new_deaths_per_mio as new_deaths_per_mio,
+    covid_data.people_fully_vaccinated_per_hundred as people_fully_vaccinated_per_hundred,
+    covid_data.extreme_poverty as extreme_poverty,
+    covid_data.excess_mortality as excess_mortality,
+    covid_data.human_development_index as hdi,
+    covid_data.continent,
+    covid_data.country
+
+from covid_data
+
+where country = 'Germany'
