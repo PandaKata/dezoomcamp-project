b65a4b7b48a1830035a2c291505dd0e67a8a8214
kataeichinger@gmail.com <kataeichinger@gmail.com>
2023-03-28 09:30:34 +0000

    removed partitions

--- dbt/models/staging/stg_covid_project.sql
+++ dbt/models/staging/stg_covid_project.sql
@@ -1,9 +1,5 @@
 {{ config(
-    materialized='table',
-    partition_by={
-      "field": "date",
-      "data_type": "timestamp",
-      "granularity": "day"
+    materialized='table'
     }
 )}}
 
